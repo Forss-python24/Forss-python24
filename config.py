b692eff862e814923a8a4fd4fbdead33510e28ae
Forss-python24 <93495327+Forss-python24@users.noreply.github.com>
2021-11-03 22:38:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,2 @@
-PAGE_ACCESS_TOKEN = 'EAAI4mipBnY4BABla6GQS4Ie0RAcgoYIr63FoWQzmUrgSx99SjSYwoL05ZAe1Ta9Qx9xGQ94x3tGCMzHhlkPUOPi7XpQZAaKeH0xnZC7Ha1ZB0AOuDSN0rlQd02Jgp6cyLK1xzuwAGmPi8gZC4nyWsTKVeayaG6TGlxa9O9j9TyReRFgJBzvkTFwcuZBhpoaMsZD'
+PAGE_ACCESS_TOKEN = None
 
-
-
-def func(**kwargs):
-
-    i = 0
-    x = len(kwargs)
-
-    print(kwargs)
-    buttons = []
-    while i<x:
-
-        buttons.append(kwargs)
-        i += 3
-
-    return buttons
-
-print(func(type='postback', title="details", payload='bt1'))
